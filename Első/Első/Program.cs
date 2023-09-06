f2a08b69f598afd303d4ebbed2fb686b2f4d9060
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-09-06 10:34:33 +0200

    b

---
+++ Első/Első/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Első
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Vilag!"[2]);
+            int szam = 6;
+            Console.WriteLine(szam);
+            double masodik = 1.2;
+            Console.WriteLine(masodik);
+            char kar = 'k';
+            Console.WriteLine(kar);
+
+        }
+    }
+}
