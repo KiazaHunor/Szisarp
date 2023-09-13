10ef52e69175b6c332b6f768723fca293bebab96
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-09-13 12:32:52 +0200

    a

---
+++ 09. 13 ÓRA/09. 13 ÓRA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._13_ÓRA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = 0;
+            while ( szam < 999999)
+            { 
+               Console.Write("Adjon meg egy szamot: ");
+               szam = Convert.ToInt32(Console.ReadLine());
+
+                if (szam >= 100000)
+                {
+                    Console.WriteLine("A szam 6 jegyű.");
+                }
+                else if (szam >= 10000)
+                {
+                    Console.WriteLine("A szam 5 jegyű!");
+                }
+                else if (szam >= 1000)
+                {
+                    Console.WriteLine("A szam 4 jegyű");
+                }
+                else if (szam >= 100)
+                {
+                    Console.WriteLine("A szam 3 jegyű!");
+                }
+                else if (szam >= 10)
+                {
+                    Console.WriteLine("A szam 2 jegyű!");
+                }
+                else 
+                {
+                    Console.WriteLine("A szam 1 jegyű!");
+                }
+            }   
+        
+
+
+
+
+        }
+    }
+}
