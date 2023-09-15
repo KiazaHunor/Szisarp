cde2c88eff06fc932a649a7b2ca8b5bab902aed4
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-09-15 11:29:34 +0200

    a

---
+++ 09.15 Órai munka/09.15 Órai munka/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._13_ÓRA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg egy szamot");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < szam; i++)
+            {
+                Console.WriteLine(i * 10);
+            }
+
+
+            // Masodik feladat
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(i * k + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Adjon meg egy szamot: ");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= szam2; i++)
+            {
+                for (int k = 0; k < szam2; k++)
+                {
+                    Console.Write(i * k + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+        }   
+
+
+    }
+}
