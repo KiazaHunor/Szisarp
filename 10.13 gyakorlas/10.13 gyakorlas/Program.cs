2ae12d08627822cfa977b1595d2a3f283cd87fcf
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-10-17 11:27:31 +0200

    a

--- 10.13 gyakorlas/10.13 gyakorlas/Program.cs
+++ 10.13 gyakorlas/10.13 gyakorlas/Program.cs
@@ -50,7 +50,7 @@ for (int i = 0; i < szavak.Length; i++)
     }
 }
 */
-
+/*
 string[] szavak = new string[] { "alma", "körte",
                 "banán", "barack", 
                 "kivi", "dinnye" };
@@ -80,3 +80,80 @@ for (int i = 0; i < hosszu.Length; i++)
 {
     Console.WriteLine(hosszu[i]);
 }
+*/
+
+// 1000 ELEMŰ TÖMB 3 jegyu szamokkal.
+
+//1000 3 jegyű random szám
+int[] szamok = new int[1000];
+Random rand = new Random();
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = rand.Next(100, 1000);
+}
+foreach (int s in szamok)
+{
+    Console.WriteLine(s);
+}
+/*
+0. sor: 2-vel osztható
+1. sor: 3-mal osztható
+2. sor: 4-gyel osztható
+3. sor: 5-tel osztható
+4. sor: 6-tal osztható
+*/
+int[,] oszthato = new int[5, 1000];
+int sor0 = 0;
+int sor1 = 0;
+int sor2 = 0;
+int sor3 = 0;
+int sor4 = 0;
+for (int i = 0; i < oszthato.GetLength(1); i++)
+{
+    if (szamok[i] % 2 == 0)
+    {
+        oszthato[0, sor0++] = szamok[i];
+    }
+}
+for (int i = 0; i < oszthato.GetLength(1); i++)
+{
+    if (szamok[i] % 3 == 0)
+    {
+        oszthato[1, sor1++] = szamok[i];
+    }
+}
+for (int i = 0; i < oszthato.GetLength(1); i++)
+{
+    if (szamok[i] % 4 == 0)
+    {
+        oszthato[2, sor2++] = szamok[i];
+    }
+}
+for (int i = 0; i < oszthato.GetLength(1); i++)
+{
+    if (szamok[i] % 5 == 0)
+    {
+        oszthato[3, sor3++] = szamok[i];
+    }
+}
+for (int i = 0; i < oszthato.GetLength(1); i++)
+{
+    if (szamok[i] % 6 == 0)
+    {
+        oszthato[4, sor4++] = szamok[i];
+    }
+}
+foreach (int s in oszthato)
+{
+    Console.WriteLine(s);
+}
+
+int kettoveloszthato = 0;
+for (int i = 0; i < oszthato.GetLength(1); i++)
+{
+    if (oszthato[0, i] != 0)
+    {
+        kettoveloszthato++;
+    }
+}
+Console.WriteLine(kettoveloszthato);
---
+++ 10.17 orai gyak/10.17 orai gyak/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel.Design;
+
+int[] tombKeszit(int darab,int legkisebb, int legnagyobb) 
+{
+    int[] tomb=new int[darab];
+    Random rand= new Random();
+    for (int i = 0; i < tomb.Length; i++)
+    {
+        tomb[i] = rand.Next();
+    }
+    return tomb;    
+}
+int[] szamok=tombKeszit(1000, 100, 999);
+
+int a = 0;
+int b = 0;
+/*
+for (int i = 0; i < szamok.Length; i++) 
+{
+    if (szamok[i] % 2 == 0) 
+    {
+        a += 1;
+    }
+    else
+    {
+        b += 1;
+    }
+}
+Console.WriteLine("Páros:" + a);
+Console.WriteLine("Paratlan" + b);
+*/
+//következő elem vizsgálata
+/*
+bool vanEgyenloSzomszedos=false;
+for (int i = 1; i < szamok.Length; i++)
+{
+    if (szamok[i] == szamok[i -1])
+    {
+       vanEgyenloSzomszedos = true;
+        //break;
+        Console.WriteLine("Egyenlő:{0} és {1},{2} = {3}", i-1, i, szamok[i - 1], szamok[i]);
+    }
+}
+if (vanEgyenloSzomszedos)
+{
+    Console.WriteLine("Van egyenlő szomszédos eleme!");
+}
+else 
+{
+    Console.WriteLine("Van egyenlő szomszédos eleme!");
+}
+*/
+/*
+for(int i = 1; i < szamok.Length;i++)
+{
+    if (szamok[i] > szamok[i] + 1)
+    {
+        Console.WriteLine(szamok[i] > szamok[1]-1);
+    }
+    else 
+    {
+        Console.WriteLine("Nem jó");
+    }
+}
+*/
+
+void szakasz2(int[] szamTomb) 
+{
+    int kezd = 0;
+    int veg = 0;
+    int Maxkezd = 0;
+    int Maxveg=0;
+
+    for (int i = 1;i < szamTomb.Length;i++) 
+    {
+        if (szamTomb[i - 1] < szamTomb[i])
+        {
+
+        }
+        else 
+        {
+            veg = i - 1;
+            if (veg - kezd + 1 > Maxveg - Maxkezd + 1)
+            {
+                Maxveg = veg;
+                Maxkezd = kezd;
+            }
+            kezd = i;
+        }
+    }
+    Console.WriteLine("A legnagyobb szamsorozat {0}" ,Maxveg - Maxkezd);
+    for (int i = Maxkezd; i < Maxveg + 1; i++)
+    {
+        Console.Write(szamTomb[i]);
+    }
+    Console.WriteLine();
+}
+szakasz2(szamok);
