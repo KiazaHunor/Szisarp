a3009301fe81e509760c1ac9275fc013070c0354
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-11-20 14:04:23 +0100

    a

--- 11.17 projekt/11.17 projekt/Program.cs
+++ 11.17 projekt/11.17 projekt/Program.cs
@@ -7,7 +7,7 @@ int szamok = rand.Next(104);
 
 for (int i = 0; i < szamok; i++)
 {
-    if (dealer.Count < 3)
+    if (dealer.Count < 2)
     {
         dealer.Add(rand.Next(1, 12));
     }
@@ -19,7 +19,7 @@ for (int i = 0; i < szamok; i++)
 
 for (int i = 0; i < szamok; i++)
 {
-    if (player.Count < 3)
+    if (player.Count < 2)
     {
         player.Add(rand.Next(1, 12));
     }
@@ -41,4 +41,16 @@ foreach (int i in player)
 
 //Összeadása elemeknek pl: 2+2=4
 
-Console.WriteLine(dealer.AsQueryable().Sum())
+int sum = dealer.AsQueryable().Sum();
+
+Console.WriteLine();
+Console.WriteLine("Az dealer első 2 lapjának értéke {0}",sum);
+
+//player változat
+
+int sum2 = player.AsQueryable().Sum();
+
+Console.WriteLine();
+Console.WriteLine("Az első 2 lapod értéke {0}",sum2);
+
+
