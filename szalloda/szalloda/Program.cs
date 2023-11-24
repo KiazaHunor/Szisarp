c5fd64f8d2709212a30f6ab08c202560f072fdb1
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-11-24 11:29:58 +0100

    a

--- szalloda/szalloda/Program.cs
+++ szalloda/szalloda/Program.cs
@@ -1,13 +1,13 @@
 ﻿//2011 Pitypang
 
-static int fizetendo(int erkezes, int tavozas, int szemelyDb,bool etkezes) 
+static int fizetendo(int erkezes, int tavozas, int szemelyDb,bool etkezes)
 {
     int szobaAr = 0;
     if (erkezes < 121) szobaAr = 9000;
     else if (erkezes < 244) szobaAr = 1000;
     else szobaAr = 8000;
 
-    int ejDb = tavozas -erkezes;
+    int ejDb = tavozas - erkezes;
     if (szemelyDb > 3)
     {
         szobaAr += 2000;
@@ -15,12 +15,21 @@ static int fizetendo(int erkezes, int tavozas, int szemelyDb,bool etkezes)
     if (etkezes)
         szobaAr += szemelyDb * 1100;
 
-    return szobaAr*ejDb;
-        
-    
+    return szobaAr * ejDb;
+
+
+
 }
-
-
+static int melyikHonap(int napDb,List<int>holKezdodoik) 
+{
+    for(int i = 0; i<holKezdodoik.Count; i++) 
+    {
+        if (napDb <= holKezdodoik[i]) 
+        {
+            return i + 1;
+        }
+    }
+}
 
 
 
@@ -68,11 +77,58 @@ for (int i = 0; i < sorszamok.Count; i++)
 Console.WriteLine("Masodik feladat");
 Console.WriteLine("{0} ({1}-{2})", vendeg[maxId],erkezesNapja[maxId],maxEj );
 
+int osszeg = 0;
+
 StreamWriter ir = new StreamWriter("bevetel.txt");
 
 for(int i = 0;i < sorszamok.Count; i++) 
 {
-    ir.WriteLine("{0}:{1}", sorszamok[i], fizetendo(erkezesNapja[i], tavozasNapja[i], vendegSzam[i], etkezes[i]));
+    int fiz = fizetendo(erkezesNapja[i], tavozasNapja[i], vendegSzam[i], etkezes[i]);
+    ir.WriteLine("{0}:{1}", sorszamok[i], fiz);
+    
+
+    osszeg = osszeg + fiz;
+        
+
 }
 
 ir.Close();
+Console.WriteLine("Az éves bevétele {0} Ft volt.",osszeg);
+
+
+olvas = new StreamReader("honapok.txt");
+List<string> honapok = new List<string>();
+List<int> napszam = new List<int>();
+List<int> holKezdodik = new List<int>();
+
+int mutato = 0;
+
+while (olvas.EndOfStream) 
+{
+    string sor= olvas.ReadLine();
+    if(mutato== 0) 
+    {
+        honapok.Add(sor);
+    }
+    else if(mutato== 1) 
+    {
+        napszam.Add(Convert.ToInt32(sor));
+    }
+    else
+    {
+        holKezdodik.Add(Convert.ToInt32(sor));
+        mutato = -1;
+    }
+
+    mutato++;
+}
+
+
+Dictionary<int,int> vendegejszakak = new Dictionary<int,int>();
+
+for (int i = 0; i < sorszamok.Count; i++) 
+{
+    Console.WriteLine(erkezesNapja[i] + "-" + tavozasNapja[i]);
+}
+
+Console.WriteLine(melyikHonap(1, holKezdodik));
