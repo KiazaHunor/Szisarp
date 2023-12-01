5212d5b8bc0e746f21b0b7c41a18f33de3a4e278
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-12-01 08:45:45 +0100

    a

---
+++ Dolgozat/Dolgozat/Program.cs
@@ -0,0 +1,26 @@
+﻿//Első feladat.
+
+
+string[] beolvas = File.ReadAllLines("adathalmas.csv");
+
+
+
+
+
+List<string> elso = new List<string>();
+List<string> masodik = new List<string>();
+List<string> harmadik = new List<string>();
+
+string sor = beolvas[];
+string[] igen = sor.Split(";");
+
+Console.WriteLine(elso);
+// Második feladat
+
+/*
+for (int i = 1; i < beolvas.Length; i++)
+{ 
+    
+    
+}
+*/
---
+++ Jázmin1/Jázmin1/Program.cs
@@ -0,0 +1,177 @@
+﻿using System.ComponentModel.Design;
+bool kezdo = true;
+while (kezdo)
+{
+
+
+
+
+
+    List<int> dealer = new List<int>();
+    List<int> player = new List<int>();
+
+    Random rand = new Random();
+
+    int szamok = rand.Next(104);
+
+    for (int i = 0; i < szamok; i++)
+    {
+        if (dealer.Count < 2)
+        {
+            dealer.Add(rand.Next(1, 12));
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    for (int i = 0; i < szamok; i++)
+    {
+        if (player.Count < 2)
+        {
+            player.Add(rand.Next(1, 12));
+        }
+        else
+        {
+            break;
+        }
+    }
+
+
+
+    //Összeadása elemeknek pl: 2+2=4
+
+    int summ = dealer.AsQueryable().Sum();
+    /*
+    Console.WriteLine();
+    Console.WriteLine(summ);
+    */
+    //player változat
+    int summ2 = player.AsQueryable().Sum();
+    /*
+    Console.WriteLine();
+    Console.WriteLine(summ2);
+    */
+    Console.WriteLine("---------------------BLACKJACK---------------------------");
+    Console.WriteLine("A játékos lapjainak összege: {0}", summ2);
+
+    //jaték
+
+    Console.WriteLine("A dealer lapjai: {0}, ?", dealer[0]);
+    Console.WriteLine("Mit szeretne tenni? (stand, hit)");
+    string dontes = Console.ReadLine();
+
+    while (dontes != "")
+    {
+        if (dontes == "igen")
+        {
+            for (int i = 0; i < szamok; i++)
+            {
+                if (dealer.Count < 2)
+                {
+                    dealer.Add(rand.Next(1, 12));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < szamok; i++)
+            {
+                if (player.Count < 2)
+                {
+                    player.Add(rand.Next(1, 12));
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if (dontes == "stand")
+        {
+            Console.WriteLine("Az ön lapjai: {0}", summ2);
+            while (summ < 16)
+            {
+                int randomszam = rand.Next(1, 12);
+                summ += randomszam;
+            }
+            Console.WriteLine("Dealer lapjai: {0}", summ);
+
+
+
+            if (summ > 21)
+            {
+                Console.WriteLine("Ön nyert");
+                break;
+            }
+            if (summ == 21)
+            {
+                Console.WriteLine("Enemy BlackJack, Vesztettél");
+                break;
+            }
+            else if (summ > summ2)
+            {
+                Console.WriteLine("A dealer nyert.");
+                break;
+            }
+
+            else if (summ2 == summ)
+            {
+                Console.WriteLine("Draw.");
+                break;
+            }
+            else if (summ2 > summ)
+            {
+                Console.WriteLine("Ön nyert.");
+                break;
+            }
+        }
+
+
+
+        if (dontes == "hit")
+        {
+            player.Add(rand.Next(1, 12));
+            summ2 = player.AsQueryable().Sum();
+
+        }
+
+
+        Console.WriteLine("Az ön lapjai összege: {0}", summ2);
+
+        if (summ2 == 21)
+        {
+            Console.WriteLine("BLACKJACK, Nyertél");
+            break;
+        }
+
+        if (summ2 > 21)
+        {
+            Console.WriteLine("BUST");
+            break;
+        }
+
+        Console.WriteLine("Mit szeretne tenni? (stand, hit)");
+        dontes = Console.ReadLine();
+    }
+
+    Console.WriteLine("Szeretne mégegy kört? (igen, nem)");
+    dontes = Console.ReadLine();
+
+    if (dontes == "igen")
+    {
+        dealer.Clear();
+        player.Clear();
+        kezdo = true;
+        Console.WriteLine("--------------------------");
+    }
+    else
+    {
+        Console.WriteLine("Rendben. Viszlát!");
+        kezdo = false;
+    }
+}
