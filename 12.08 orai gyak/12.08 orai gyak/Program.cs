e47aa69e7b577d8b0525851ff5e31b503ea7e4c5
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2023-12-12 14:05:30 +0100

    a

---
+++ 12.08 orai gyak/12.08 orai gyak/Program.cs
@@ -0,0 +1,21 @@
+﻿List <string> egy = new List<string>();
+List<string> ketto = new List<string>();
+
+StreamReader olvas = new StreamReader("kiserlet.txt");
+
+while (!olvas.EndOfStream) 
+{
+    egy.Add(olvas.ReadLine());
+}
+
+
+for (int i = 0; i < egy.Count; i++) 
+{
+    Console.WriteLine(egy[i]);
+}
+
+olvas.Close();
+
+
+File.ReadAllLines("kiserlet.txt");
+
