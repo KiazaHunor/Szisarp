fd25209e537e67edbb1882429a90375d762c5332
KiazaHunor <98027608+KiazaHunor@users.noreply.github.com>
2024-01-13 16:11:42 +0100

    Add files via upload

---
+++ fordítva.cs
@@ -0,0 +1,27 @@
+﻿
+List<int> szamok = new List<int>();
+
+Console.Write("1. szam:");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+Console.Write("2. szam:");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+Console.Write("3. szam:");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+Console.Write("4. szam:");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+
+
+
+
+szamok.Reverse();
+
+Console.WriteLine();
+
+foreach (int szam in szamok) 
+{
+    Console.WriteLine(szam);
+}
+
+szamok.Reverse(0, 3);
+
+
