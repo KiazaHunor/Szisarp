b993d4878cce70d73802275af60ae0de4e8d5340
KiazaHunor <98027608+KiazaHunor@users.noreply.github.com>
2024-01-14 19:15:50 +0100

    Add files via upload

---
+++ forditva2.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Kérem, adjon meg egy karaktersorozatot: ");
+
+string beker = Console.ReadLine();
+
+char[] charArray = beker.ToCharArray();
+
+Array.Reverse(charArray);
+
+Console.WriteLine("A bekért karaktersorozat fordítva: " + new string(charArray));
