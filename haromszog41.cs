c6674bfe9b4fe1c380be84b99a807eef299a98e8
KiazaHunor <98027608+KiazaHunor@users.noreply.github.com>
2024-01-14 14:03:55 +0100

    Add files via upload

---
+++ haromszog41.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("A program neve :haromszog4");
+Console.WriteLine("Ez a program egy háromszöget vizsgál!");
+
+
+
+Console.Write("1.oldal:");
+double a =Convert.ToDouble(Console.ReadLine());
+Console.Write("2.oldal:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("3.oldal:");
+double c = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+// Console.WriteLine(oldalak[1]);
+
+if (a + b >c && a + c > b && b+c > a)
+{
+    Console.WriteLine("Működik!");
+}
+
+else
+{
+    Console.WriteLine("nem háromszög!");
+}
+
+double s = (a + b + c) / 2;
+double terulet = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+Console.WriteLine("A háromszög területe: " + terulet + "cm2");
