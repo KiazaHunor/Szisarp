8fd076bcc952994d65ef71be36a5636495dd6a41
KiazaHunor <98027608+KiazaHunor@users.noreply.github.com>
2024-01-14 13:37:40 +0100

    Add files via upload

---
+++ szultes.cs
@@ -0,0 +1,12 @@
+﻿
+int elso = (7650);
+int masodik = (7490);
+int harmadik = (7590);
+int negyedik = (8020);
+int otodik = (7720);
+
+Console.WriteLine("| " + "2012 " + "| " + elso + "fő | ");
+Console.WriteLine("| " + "2013 " + "| " + masodik + "fő | ");
+Console.WriteLine("| " + "2014 " + "| " + harmadik + "fő | ");
+Console.WriteLine("| " + "2015 " + "| " + negyedik + "fő | ");
+Console.WriteLine("| " + "2016 " + "| " + otodik + "fő | ");
