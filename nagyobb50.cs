eb88459010fff26cd5f50d86796b345c952936d5
KiazaHunor <98027608+KiazaHunor@users.noreply.github.com>
2024-01-14 10:34:06 +0100

    Add files via upload

---
+++ nagyobb50.cs
@@ -0,0 +1,26 @@
+﻿//1.megoldás (nem működik!)
+
+Console.Write("Adjon meg egy számot 10-100 között:");
+int szam = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < szam; i++) 
+{
+    if (szam > 50)
+    {
+        Console.WriteLine("Oks!");
+    }
+    else if (szam < 50) 
+    {
+        Console.WriteLine("Próbálja meg újra:");
+    }
+}
+
+
+//2.megoldás (Netes segítséggel!)
+int number;
+
+do
+{
+    Console.Write("Kérlek, add meg egy számot 10-100 között:");
+} 
+while (!int.TryParse(Console.ReadLine(), out number) || number < 50);
