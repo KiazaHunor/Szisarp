b0d90075c519d0d654eb41422b9e71046be55805
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2024-01-23 14:01:21 +0100

    adw

---
+++ Uzemanyag/Uzemanyag/adatok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzemanyag
+{
+    internal class adatok
+    {
+        public DateTime datum;
+        public int benzin;
+        public int gazolaj;
+
+        public adatok(string sor) 
+        {
+            string[] vag = sor.Split(';');
+            datum = Convert.ToDateTime(vag[0]);
+            benzin = int.Parse(vag[1]);
+            gazolaj = int.Parse(vag[2]);
+        }
+        public int kulonbseg()
+        {
+            return Math.Abs(benzin -gazolaj);
+        }
+    }
+
+}
