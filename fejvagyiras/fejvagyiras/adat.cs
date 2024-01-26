e67894616e8c83799a681fb0a2a49b2eeed3e92b
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2024-01-26 08:45:50 +0100

    a

---
+++ fejvagyiras/fejvagyiras/adat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fejvagyiras
+{
+    internal class adat
+    {
+        public string dobas;
+        public int db;
+
+        public adat(string dobas) 
+        {
+            this.dobas = dobas;
+            db = 1;
+        }
+    }
+}
