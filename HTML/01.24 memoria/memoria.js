d38d59629590330b212d065281df2710aafc6daa
Horváth Hunor <horvathh21@ADY-NAGYATAD.SULINET.HU>
2024-01-26 13:20:38 +0100

    a

--- HTML/01.24 memoria/memoria.js
+++ HTML/01.24 memoria/memoria.js
@@ -1,19 +1,60 @@
-var kepUrl=["l.jpeg","a.jpeg","b.jpeg","l.jpeg"]
+var kepekUrl=["a.jpg","b.jpg","c.jpeg","d.jpeg","e.jfif","f.jfif","k.jfif","t.jfif","l.png"]
+var parDb=4;
 
 function init()
 {
-    //console.log("hallo");
+    //console.log("hahó");
+    kepKirakas();
 }
 
-function keokirakas()
+var kattintas=0;
+
+function kepKirakas()
 {
     let asztal=document.getElementById("asztal");
+    let kartyak=[]
 
-
-    for(let i=0; i<kepUrl.length();i++);
+    for(let k=0;k<2;k++)
     {
-    let uj=document.createElement("img");
-    uj.src="kepek/"+ kepUrl[i];
+        for(let i=0;i<parDb;i++)
+        {
+
+            let uj=document.createElement("div"); //<div></div>
+    
+            uj.className="kartya";
+            uj.onclick=function()
+            {
+              uj.style.backgroundImage="url(kepek/"+ kepekUrl[i]+")";
+              kattintas++;
+            }
+            
+            kartyak.push(uj)
+            
+        }
     }
-    asztal.appendChild(uj);
+    kartyak=kever(kartyak);
+
+    for(let i=0;i<kartyak.length;i++)
+    {
+      asztal.appendChild(kartyak[i]);
+    }
+    
+    
+
+
+  function kever(points) 
+{
+  for (let i = points.length -1; i > 0; i--) 
+  {
+    let j = Math.floor(Math.random() * (i+1));
+    let k = points[i];
+    points[i] = points[j];
+    points[j] = k;
+  }
+  return points;
+  
+}
+
+
+
 }
