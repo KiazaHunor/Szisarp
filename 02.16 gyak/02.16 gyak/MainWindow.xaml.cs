9e027e723fb5a53a55fb2adf15cf1598d8a32a59
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-02-20 14:05:11 +0100

    a

---
+++ 02.16 gyak/02.16 gyak/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02._16_gyak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Levi.Visibility == Visibility.Visible )
+            {
+            Levi.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Levi.Visibility=Visibility.Visible;
+            }
+        }
+
+        private void Button_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Levi.Opacity = slider.Value;
+        }
+    }
+}
---
+++ GUI 0220. vol 2/GUI 0220. vol 2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_0220._vol_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int osszeg =Convert.ToInt32(textbox1.Text) + Convert.ToInt32(textbox2.Text);
+            textblock.Text = osszeg.ToString();
+
+            textblock1.Text = textbox1.Text+"+"+ textbox2.Text+"="+ osszeg.ToString()+"\n"+ textblock1.Text;
+        }
+    }
+}
