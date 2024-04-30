1b331e43431f3c16ac32d1395054e6def8e0e99e
Horváth Hunor <horvathh21@ady-nagyatad.sulinet.hu>
2024-04-30 14:05:27 +0200

    D

---
+++ 04.30 gyakorlas D/04.30 gyakorlas D/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04._30_gyakorlas_D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void mezo(int sor, int oszlop, string tartalom)
+        {
+            Label cimke = new Label();
+            cimke.Content = "awdaw";
+            cimke.BorderBrush = Brushes.SlateBlue;
+            cimke.BorderThickness= new Thickness(5);
+            cimke.Background = Brushes.AliceBlue;
+            cimke.Foreground = Brushes.White;
+            cimke.Height = 20;
+            cimke.Width = 20;
+            cimke.Margin=new Thickness(cimke.Width*oszlop, cimke.Height*sor,0,0);
+            cimke.HorizontalAlignment = HorizontalAlignment.Left;
+            cimke.VerticalAlignment = VerticalAlignment.Top;
+            tabla.Children.Add(cimke);
+            
+        }
+      
+    }
+}
